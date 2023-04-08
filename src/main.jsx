f56db154503ea5178d9b997a2f02fd33bd191f1e
Carlos Rincón <carlos.rincon@uniagustiniana.edu.co>
2023-04-08 17:54:16 -0500

    fixing TodoContext issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
-
 import { TodoProvider } from "./TodoContext"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
